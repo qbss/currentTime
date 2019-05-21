68345f9969b84ed9e8958c98d216800672f4959c
qbss <jfritschle@hotmail.com>
2019-05-21 02:45:23 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace currentTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The current time is " + DateTime.Now);
+        }
+    }
+}
